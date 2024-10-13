0bb1f234e7a57fb722c6d4f60fba34389e5e5aa1
Daniil Cheremisin <daniel.sugar@yandex.ru>
2024-10-14 00:46:07 +0300

    Fixed api origin

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+export const API_ORIGIN =
+	window.location.hostname === 'ovrdrve.github.io'
+		? 'https://larek-api.nomoreparties.co'
+		: process.env.API_ORIGIN;
+export const API_URL = `${API_ORIGIN}/api/weblarek`;
+export const CDN_URL = `${API_ORIGIN}/content/weblarek`;
 
-export const settings = {
-
-};
+export const settings = {};
